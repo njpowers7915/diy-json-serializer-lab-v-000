ab7d2745b85fa613f113ba89f66ee7c2eb3b63c5
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 13:07:48 +0000

    automatically backed up by learn

--- app/models/serializers/product_serializer.rb
+++ app/models/serializers/product_serializer.rb
@@ -8,7 +8,7 @@ class ProductSerializer < ActiveRecord::Base
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price.to_s + '", '
     serialized_product += '"inventory": "' + product.inventory.to_s + '", '
-    serialized_product += '"description": "' + product.description + '" '
+    serialized_product += '"description": "' + product.description + '"'
 
     # and end with the close brace
     serialized_product += '}'
