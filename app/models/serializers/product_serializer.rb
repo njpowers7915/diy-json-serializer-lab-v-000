c24d2ca1d8a63c5b627f3d722a6bc212ab460364
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 13:59:46 +0000

    automatically backed up by learn

--- app/models/serializers/product_serializer.rb
+++ app/models/serializers/product_serializer.rb
@@ -4,10 +4,10 @@ class ProductSerializer < ActiveRecord::Base
     # start with the open brace to create a valid JSON object
     serialized_product = '{'
 
-    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"id": ' + product.id + ', '
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
+    serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '"inventory": "' + product.inventory + '", '
     serialized_product += '"description": "' + product.description + '"'
 
     # and end with the close brace
