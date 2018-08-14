550124226664d7a05b67126f86783279ec85977e
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 12:56:42 +0000

    automatically backed up by learn

--- app/models/serializers/product_serializer.rb
+++ app/models/serializers/product_serializer.rb
@@ -6,8 +6,8 @@ class ProductSerializer < ActiveRecord::Base
 
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"price": "' + product.price + '", '
-    serialized_product += '"inventory": "' + product.inventory + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
     serialized_product += '"description": "' + product.description + '" '
 
     # and end with the close brace
